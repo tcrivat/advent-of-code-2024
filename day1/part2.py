68d7f6922bce38e22d245ab5938e325604d6fc69
Teodor Crivăț <dorucriv@gmail.com>
2024-12-01 12:16:34 +0200

    Update part2.py to use a defaultdict

--- day1/part2.py
+++ day1/part2.py
@@ -1,15 +1,17 @@
+from collections import defaultdict
+
 INPUT_FILE = "input.txt"
 
 def solution(filename):
-    d1, d2 = {}, {}
+    f1, f2 = defaultdict(int), defaultdict(int)
     with open(filename) as f:
         for line in f:
             n1, n2 = map(int, line.split())
-            d1[n1] = d1.get(n1, 0) + 1
-            d2[n2] = d2.get(n2, 0) + 1
+            f1[n1] += 1
+            f2[n2] += 1
     answer = 0
-    for n in d1:
-        answer += n * d1[n] * d2.get(n, 0)
+    for n in f1:
+        answer += n * f1[n] * f2[n]
     return answer
 
 if __name__ == "__main__":
