9e027ce98ab9b59164b3e31fecf6eadb9a9c4610
Teodor Crivăț <dorucriv@gmail.com>
2024-12-03 10:23:27 +0200

    Update part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -4,7 +4,7 @@ INPUT_FILE = "input.txt"
 
 def solution(filename):
     with open(filename) as f:
-        program = "".join(line.strip() for line in f)
+        program = f.read().replace("\n", "")
     matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", program)
     return sum(int(n1) * int(n2) for n1, n2 in matches)
 
