71632ca979d018c57bfa8c4930d9c49d20b518d2
Teodor Crivăț <dorucriv@gmail.com>
2024-12-11 17:37:46 +0100

    Update part2.py

--- day11/part2.py
+++ day11/part2.py
@@ -7,15 +7,14 @@ def evolve(stone, generation):
         return 1
     if (stone, generation) in memo:
         return memo[(stone, generation)]
-    stones = []
+    answer = 0
     if stone == "0":
-        stones.append("1")
+        answer += evolve("1", generation - 1)
     elif len(stone) % 2 == 0:
-        stones.append(stone[0 : len(stone) // 2])
-        stones.append(str(int(stone[len(stone) // 2 :])))
+        answer += evolve(stone[0 : len(stone) // 2], generation - 1)
+        answer += evolve(str(int(stone[len(stone) // 2 :])), generation - 1)
     else:
-        stones.append(str(int(stone) * 2024))
-    answer = sum(evolve(stone, generation - 1) for stone in stones)
+        answer += evolve(str(int(stone) * 2024), generation - 1)
     memo[(stone, generation)] = answer
     return answer
 
